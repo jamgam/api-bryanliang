93ed093c43aee5f3c181f1b7cb9c18a52c9c4abb
Bryan Liang <bryanliang1995@gmail.com>
2021-02-14 00:34:43 -0800

    Use package lock

--- index.js
+++ index.js
@@ -1,72 +1,13 @@
-const dotenv = require('dotenv')
-const cors = require('cors')
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const schema = require('./lib/schema')
-// const root = require('./lib/root')
-const hash = require('object-hash')
-const fetch = require('node-fetch')
+import dotenv from 'dotenv'
+import cors from 'cors'
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import schema from './lib/schema'
+import rootValue from './lib/root'
+import hash from 'object-hash'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
-
-const config = {
-  headers: {
-    'Hasura-Client-Name': 'api-bryanliang-console',
-    'Content-Type': 'application/json',
-    'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET
-  },
-}
-
-const graphql = async (query) => {
-  const response = await fetch(process.env.HGE_ENDPOINT + '/graphql', {
-    method: 'post',
-    body:    JSON.stringify({query}),
-    ...config,
-  })
-  const jsonResp = await response.json()
-  return jsonResp?.data
-}
-
-const rootValue = {
-  getHighscores: async () => {
-    const { Score } = await graphql(`query {
-      Score(limit: 10, order_by: {score: desc}) {
-        date
-        duration
-        id
-        score
-        username
-      }
-    }`)
-    return Score
-  },
-  createScore: async ({duration, score, username}) => {
-    const { insert_Score_one } = await graphql(`mutation {
-      insert_Score_one(object: {duration: ${duration}, score: ${score}, username: "${username}"}) {
-        id
-        score
-        username
-        duration
-        date
-      }
-    }`)
-    return insert_Score_one
-  },
-  updateScore: async ({id, username}) => {
-    const { update_Score_by_pk } = await graphql(`mutation {
-      update_Score_by_pk(pk_columns: {id: ${id}}, _set: {username: "${username}"}) {
-        id
-        date
-        duration
-        score
-        username
-      }
-    }`)
-    return update_Score_by_pk
-  }
-}
 
 app.use(cors())
 app.use(express.json())
