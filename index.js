9d0fd8b480f69bf4ac837d18b1cf0ef4dfd6f57f
Bryan Liang <bryanliang1995@gmail.com>
2021-02-14 01:28:12 -0800

    Test

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
-import schema from './lib/schema'
-import rootValue from './lib/rootValue'
+import { schema, rootValue } from './lib/schema'
 import hash from 'object-hash'
 
 const app = express()
@@ -13,25 +12,25 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send({name: 'api.bryanliang.me', version: '1.0.0', mode: process.env.NODE_ENV, test: rootValue }));
+app.get('/', (req, res) => res.send({name: 'api.bryanliang.me', version: '1.0.0', mode: process.env.NODE_ENV }));
 
-// app.use('/graphQl', (req, res, next) => {
-//   const { query, verificationHash } = req.body
-//   const correctHash = hash({query, key: process.env.SECRET_KEY})
-//   if (verificationHash === correctHash) {
-//     next()
-//   } else {
-//     console.log('HASH MISMATCH', verificationHash, correctHash)
-//     res.sendStatus(401)
-//   }
-// })
+app.use('/graphQl', (req, res, next) => {
+  const { query, verificationHash } = req.body
+  const correctHash = hash({query, key: process.env.SECRET_KEY})
+  if (verificationHash === correctHash) {
+    next()
+  } else {
+    console.log('HASH MISMATCH', verificationHash, correctHash)
+    res.sendStatus(401)
+  }
+})
 
 app.use(
   '/graphQl',
   graphqlHTTP({
     rootValue,
     schema,
-    graphiql: process.env.NODE_ENV === 'development',
+    graphiql: true,
   })
 );
 
